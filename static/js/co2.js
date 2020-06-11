73b63186e70927e8b3848aa38ccda8b20fca2f68
Etumi <etumi12@gmail.com>
2020-06-10 21:46:09 -0400

    update layout and include CO2 Timeline

---
+++ static/js/co2.js
@@ -0,0 +1,77 @@
+var url = "/api/co2_aqi"
+
+d3.json(url).then(response=> {
+
+    console.log(response)
+
+    var currentState
+    var currentLevel = [];
+    var currentYear = []; 
+    var listofStates = [];
+
+    var allStateNames = response.map(entry => entry.state)
+    var allYear = response.map(entry => entry.year)
+    var allLevel = response.map(entry => entry.co2);
+    
+      for (var i = 0; i < allStateNames.length; i++ ){
+        if (listofStates.indexOf(allStateNames[i]) === -1 ){
+          listofStates.push(allStateNames[i]);
+        }
+      }
+      
+      function getStateData(chosenState) {
+        currentLevel = [];
+        currentYear = [];
+        for (var i = 0 ; i < allStateNames.length ; i++){
+          if ( allStateNames[i] === chosenState ) {
+            currentLevel.push(allLevel[i]);
+            currentYear.push(allYear[i]);
+          } 
+        }
+      };
+    
+    // Default State Data
+    setBubblePlot('Alabama');
+      
+    function setBubblePlot(chosenState) {
+        getStateData(chosenState);  
+    
+        var trace1 = {
+          x: currentYear,
+          y: currentLevel,
+          mode: 'lines+markers',
+          marker: {
+            size: 12, 
+            opacity: 0.5
+          }
+        };
+    
+        var data = [trace1];
+    
+        var layout = {
+          title: 'CO2 Levels across Years<br>'+ chosenState + ' Levels'
+        };
+    
+        Plotly.newPlot('plotdiv', data, layout, {showSendToCloud: true});
+    };
+      
+    var innerContainer = document.querySelector('[data-num="0"'),
+        plotEl = innerContainer.querySelector('.plot'),
+        stateSelector = innerContainer.querySelector('.statedata');
+    
+    function assignOptions(textArray, selector) {
+      for (var i = 0; i < textArray.length;  i++) {
+          var currentOption = document.createElement('option');
+          currentOption.text = textArray[i];
+          selector.appendChild(currentOption);
+      }
+    }
+    
+    assignOptions(listofStates, stateSelector);
+    
+    function updateState(){
+        setBubblePlot(stateSelector.value);
+    }
+      
+    stateSelector.addEventListener('change', updateState, false);
+});
--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("map", {
   center: [37.0902, -95.7129],
-  zoom: 6
+  zoom: 5
 });
 
 // Adding tile layer
@@ -12,35 +12,165 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+
+function setRadius(aqi){
+
+  switch(true){
+    case (aqi < 10):
+      return 1;
+      break;
+    case (aqi < 20):
+      return 2;
+      break;
+    case (aqi < 30):
+      return 4;
+      break;
+    case (aqi <= 40):
+      return 8;
+      break;
+    case (aqi <= 50):
+      return 16;
+      break;
+    case (aqi <= 60):
+      return 32;
+      break;
+    case (aqi <= 70):
+      return 64;
+      break;
+    default:
+      return 128;
+  }
+}
+
+console.log("hello")
+
 var url = "/api/pollution";
 
-d3.json(url, function(response) {
+var yearsDropDown = d3.select("#years-dropdown")
 
-  console.log(response);
+d3.json(url).then(function(response) {
 
-  var heatArray = [];
+  // populate years dropdown
+  var years = response.map(entry => entry.year);
+  years.sort(function(a, b){return a - b})
+  //console.log(years);
+  var yearsUnique = new Set(years)
+  //console.log(yearsUnique)
 
-  for (var i = 0; i < response.length; i++) {
-    var location = response[i].coord;
+  // var yearsDropDown = d3.select("#years-dropdown")
 
-    if (location) {
-      heatArray.push([location[1], location[0]]);
-    }
-  }
-
-  console.log("response")
-  console.log(heatArray);
-
-  var heat = L.heatLayer(heatArray, {
-    radius: 50,
-    blur: 35
-  }).addTo(myMap);
-
+  yearsUnique.forEach(year => {
+    yearsDropDown.append("option").text(year)
+  })
 });
 
-var circle = L.circle([37.0902, -95.7129], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 5000
-}).addTo(myMap);
+
+var selYear = 2000;
+
+function filterData(data){
+
+  return data.year == selYear
+}
+
+function createMap(){
+  d3.json(url).then(function(response) {
+
+    // populate years dropdown
+    // var years = response.map(entry => entry.year);
+    // years.sort(function(a, b){return a - b})
+    // //console.log(years);
+    // var yearsUnique = new Set(years)
+    // //console.log(yearsUnique)
+
+    // // var yearsDropDown = d3.select("#years-dropdown")
+
+    // yearsUnique.forEach(year => {
+    //   yearsDropDown.append("option").text(year)
+    // })
+
+    // console.log(response);
+
+    // console.log(d3.extent(response, d => d.no2));
+    // console.log(d3.extent(response, d => d.so2));
+    // console.log(d3.extent(response, d => d.o3));
+    // console.log(d3.extent(response, d => d.co));
+
+    // console.log([response[0].coord[0], response[0].coord[1]])
+
+
+    var filteredResponse = response.filter(filterData)
+    console.log(filteredResponse);
+
+    // if (typeof circleCO !== 'undefined') {
+    //   // myMap.remove(circleCO)
+    //   // myMap.remove(circleO3)
+    //   // myMap.remove(circleNO2)
+    //   // myMap.remove(circleSO2)
+    //   // myMap.remove(marker)
+    //   console.log("hi")
+    //   circleCO.remove()
+    //   circleO3.remove()
+    //   circleNO2.remove()
+    //   circleSO2.remove()
+    //   marker.remove()
+    // }
+    // console.log(circleCO)
+    // myMap.eachLayer(layer => {
+    //   console.log("remove layer")
+    //   // console.log(layer)
+
+    //   myMap.removeLayer(layer)
+    // })
+
+    filteredResponse.forEach(entry => {
+      var circleCO = L.circle([entry.coord[0], entry.coord[1]], {
+        color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.5,
+        radius: setRadius(entry.co)*25000
+      }).addTo(myMap);
+      
+
+      var circleO3 = L.circle([entry.coord[0], entry.coord[1]], {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.5,
+        radius: setRadius(entry.o3)*5000
+      }).addTo(myMap);
+
+      var circleSO2 = L.circle([entry.coord[0], entry.coord[1]], {
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.5,
+        radius: setRadius(entry.so2)*10000
+      }).addTo(myMap);
+
+      var circleNO2 = L.circle([entry.coord[0], entry.coord[1]], {
+        color: 'yellow',
+        fillColor: 'yellow',
+        fillOpacity: 0.5,
+        radius: setRadius(entry.no2)*15000
+      }).addTo(myMap);
+
+      var marker = L.marker([entry.coord[0], entry.coord[1]]).addTo(myMap);
+
+      marker.bindPopup("<b>" + entry.state + "</b><hr>Year: " + Math.round(entry.year) + "</b><br>CO: " + Math.round(entry.co) + "<br>SO2: " + Math.round(entry.so2) + "<br>NO2: " + Math.round(entry.no2) + "<br>O3: " + Math.round(entry.o3))
+
+    })
+
+  })
+
+}
+
+createMap()
+
+    // eventListener //
+  // --------------//
+  yearsDropDown.on("change", function() {
+    //console.log(response)
+    selYear = yearsDropDown.property("value")
+    console.log(selYear)
+
+    createMap()
+    
+  })
